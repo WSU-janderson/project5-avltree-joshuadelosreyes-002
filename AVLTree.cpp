0405975751a4ec917f6d54dc463b5268eefdb2f6
joshuadelosreyes-002 <delosreyes.3@wright.edu>
2025-11-05 16:18:40 -0500

    Starting the project.

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -1,66 +1,68 @@
 #include "AVLTree.h"
 
-#include <string>
-
 size_t AVLTree::AVLNode::numChildren() const {
-    return 0;
+	return 0;
 }
 
 bool AVLTree::AVLNode::isLeaf() const {
-    return false;
+	return false;
 }
 
 size_t AVLTree::AVLNode::getHeight() const {
-    return 0;
+	return 0;
 }
 
-bool AVLTree::removeNode(AVLNode*& current){
-    if (!current) {
-        return false;
-    }
+bool AVLTree::removeNode(AVLNode*& current) {
+	if (!current) {
+		return false;
+	}
 
-    AVLNode* toDelete = current;
-    auto nChildren = current->numChildren();
-    if (current->isLeaf()) {
-        // case 1 we can delete the node
-        current = nullptr;
-    } else if (current->numChildren() == 1) {
-        // case 2 - replace current with its only child
-        if (current->right) {
-            current = current->right;
-        } else {
-            current = current->left;
-        }
-    } else {
-        // case 3 - we have two children,
-        // get smallest key in right subtree by
-        // getting right child and go left until left is null
-        AVLNode* smallestInRight = current->right;
-        // I could check if smallestInRight is null,
-        // but it shouldn't be since the node has two children
-        while (smallestInRight->left) {
-            smallestInRight = smallestInRight->left;
-        }
-        std::string newKey = smallestInRight->key;
-        int newValue = smallestInRight->value;
-        remove(root, smallestInRight->key); // delete this one
+	AVLNode* toDelete = current;
+	size_t nChildren = current->numChildren();
+	if (current->isLeaf()) {
+		/** Case 1: We can delete the node. */ 
+		current = nullptr;
+	} else if (current->numChildren() == 1) {
+		/** Case 2: Replace current with its only child. */
+		if (current->right) {
+			current = current->right;
+		} else {
+			current = current->left;
+		}
+	} else {
+		/** Case 3: We have two children.
+		 *	Get smallest key in right subtree by
+		 *	getting right child and go left until left is null. */
+		AVLNode* smallestInRight = current->right;
 
-        current->key = newKey;
-        current->value = newValue;
+		/** I could check if smallestInRight is null,
+		 *	but it shouldn't be since the node has two children. */
+		while (smallestInRight->left) {
+			smallestInRight = smallestInRight->left;
+		}
 
-        current->height = current->getHeight();
-        balanceNode(current);
+		std::string newKey = smallestInRight->key;
+		int newValue = smallestInRight->value;
+		remove(root, smallestInRight->key); // Delete this one.
 
-        return true; // we already deleted the one we needed to so return
-    }
-    delete toDelete;
+		current->key = newKey;
+		current->value = newValue;
 
-    return true;
+		current->height = current->getHeight();
+		balanceNode(current);
+
+		return true; // We already deleted the one we needed to so return.
+	}
+
+	delete toDelete;
+
+	return true;
 }
 
 bool AVLTree::remove(AVLNode *&current, KeyType key) {
-    return false;
+	return false;
 }
 
 void AVLTree::balanceNode(AVLNode *&node) {
+
 }
--- AVLTree.h
+++ AVLTree.h
@@ -1,54 +1,54 @@
 /**
- * AVLTree.h
+ *	AVLTree.h
  */
 
 #ifndef AVLTREE_H
 #define AVLTREE_H
+
 #include <string>
 
-using namespace std;
-
 class AVLTree {
-public:
-    using KeyType = std::string;
-    using ValueType = size_t;
+	public:
+		using KeyType = std::string;
+		using ValueType = size_t;
 
-protected:
-    class AVLNode {
-    public:
-        KeyType key;
-        ValueType value;
-        size_t height;
+	protected:
+		class AVLNode {
+			public:
+				KeyType key;
+				ValueType value;
+				size_t height;
 
-        AVLNode* left;
-        AVLNode* right;
+				AVLNode* left;
+				AVLNode* right;
 
-        // 0, 1 or 2
-        size_t numChildren() const;
-        // true or false
-        bool isLeaf() const;
-        // number of hops to deepest leaf node
-        size_t getHeight() const;
+				/** Must return `0`, `1`, or `2`. */
+				size_t numChildren() const;
 
+				/** Determines if the node is a leaf node. */
+				bool isLeaf() const;
 
-    };
+				/** Number of hops to deepest leaf node. */
+				size_t getHeight() const;
+		};
 
-public:
-
-
+	public:
 
 
-    private:
-    AVLNode* root;
 
-    /* Helper methods for remove */
-    // this overloaded remove will do the recursion to remove the node
-    bool remove(AVLNode*& current, KeyType key);
-    // removeNode contains the logic for actually removing a node based on the numebr of children
-    bool removeNode(AVLNode*& current);
-    // You will implement this, but it is needed for removeNode()
-    void balanceNode(AVLNode*& node);
+	private:
+		AVLNode* root;
 
+		/* Helper methods for remove. */
+
+		/** This overloaded remove will do the recursion to remove the node. */
+		bool remove(AVLNode*& current, KeyType key);
+
+		/** `removeNode` contains the logic for actually removing a node based on the numebr of children. */
+		bool removeNode(AVLNode*& current);
+
+		/** You will implement this, but it is needed for `removeNode()`. */
+		void balanceNode(AVLNode*& node);
 };
 
-#endif //AVLTREE_H
+#endif // AVLTREE_H
